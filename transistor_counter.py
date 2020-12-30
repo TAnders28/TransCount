d034431814e1e7c3e33cdceb3813c64b97a082fb
TAnders28 <anderstom25@gmail.com>
2020-12-30 10:59:45 -0500

    10th Commit - Lots of functionality

--- transistor_counter.py
+++ transistor_counter.py
@@ -1,3 +1,6 @@
+import queue
+from typing import NewType
+
 """
 Class representing a single verilog module with interior attributes representing:
 name, transistor count, inputs, outputs, wires, inner modules, and assign statements
@@ -8,30 +11,31 @@ class module:
         first_line = mod_text[0]
 
         self.trans_count = -1
-        self.name = getParseModName(first_line)
+
+        self.name = getParseModName(first_line) # PARSE IMPLEMENTED
         self.inputs = parseModInputs(getModInputs(mod_text)) # PARSE IMPLEMENTED 
         self.outputs = parseModOutputs(getModOutputs(mod_text)) # PARSE IMPLEMENTED
-        self.wires =  parseModWires(getModWires(mod_text))
-        self.modules = parseModInnerModules(getModInnerModules(mod_text))
-        self.assigns = getModAssigns(mod_text) #!!!UNIMPLEMENTED
+        self.wires =  parseModWires(getModWires(mod_text)) # PARSE IMPLEMENTED
+        self.modules = parseModInnerModules(getModInnerModules(mod_text)) # PARSE IMPLEMENTED
+        self.assigns = getModAssigns(mod_text) #!! PARSE UNIMPLEMENTED
 
     # __str__ shows a formatted version of attributes
     def __str__(self):
         input_string = ""
         for i in self.inputs:
-            input_string += i[1] + " : width " + str(i[0]) + "\n"
+            input_string += i[0] + " : width " + str(i[1]) + "\n"
 
         input_string += "\n"
             
         output_string = ""
         for o in self.outputs:
-            output_string += o[1] + " : width " + str(o[0]) + "\n"
+            output_string += o[0] + " : width " + str(o[1]) + "\n"
 
         output_string += "\n"
         
         wire_string = ""
         for w in self.wires:
-            wire_string += w[1] + " : width " + str(w[0]) + "\n"
+            wire_string += w[0] + " : width " + str(w[1]) + "\n"
         
         wire_string += "\n"
 
@@ -40,20 +44,39 @@ class module:
         string = string + "\nWires:\n" + wire_string + "\nAssigns:\n" + str(self.assigns) + "\nModules:\n" + str(self.modules)
         return string
         
-    # Getter - currently unused
+    # Getter 
     def getName(self):
         return self.name
+    
+    # Getter for modules field
+    def getModules(self):
+        return self.modules
 
+    # Calculates transistor count
     def getTransCount(self):
+        print("\n//////////////////////\n" + "Analysis of " + self.name)
+        print("\nModules: " + str(list(enumerate(self.modules, 1))) + "\n")
+
         current_count = 0
+        error_count = 0
+        module_count = 1
+
         for module in self.modules:
-            if module[1] in known_input_independent.keys():
-                current_count += known_input_independent.get(module[1])
-            elif module[1] in known_input_dependent.keys():
-                current_count += known_input_dependent.get(module[1])[0] + known_input_dependent.get(module[1])[1]*module[0]
+            if module[0] in known_input_independent.keys():
+                current_count += known_input_independent.get(module[0])
+            elif module[0] in known_input_dependent.keys():
+                current_count += known_input_dependent.get(module[0])[0] + known_input_dependent.get(module[0])[1]*module[1]
+            elif module[0] in inner_modules.keys():
+                current_count += inner_modules.get(module[0])
             else:
-                print("error")
+                error_count += 1
+                print("Error at module #" + str(module_count))
+            module_count += 1
+
         self.trans_count = current_count
+        print("\n" + str(error_count) + " total errors  in " + self.name + "\n")
+        print("Total transistors: " + str(current_count))
+        inner_modules.update({self.name : current_count})
 
 known_input_dependent = {
         'and' : (1,1), 
@@ -63,8 +86,11 @@ known_input_dependent = {
         'xor' : (0,4)
     }
 known_input_independent = {
-        'not' : 1
+        'not' : 1,
+        'bufif1' : 6
     }
+
+inner_modules = {}
     
 """
 Method that combines and formats the lines of a verilog file, which 
@@ -87,35 +113,7 @@ def combine_lines(text):
             combined_lines.append(current.strip().replace('\n', ' '))
             current = ''
     return combined_lines
-"""
-Calculates transistor count based on interior characteristics.
-"""
-def getModTransCount(text):
 
-    transCount = 0
-    for line in text[1:-1]:
-        transCount = transCount + countTransistorsInLine(line)
-    return transCount
-    # TODO
-
-"""
-Helper method for getModTransCount.
-Counts the transistors in one line.
-
-"""
-def countTransistorsInLine(line):
-    if (line[0:5] == 'input'):
-        return 0
-    elif (line[0:6] == 'output'):
-        return 0
-    elif (line[0:4] == 'wire'):
-        return 0
-    elif (line[0:6] == 'assign'):
-        return 0
-    else: 
-        split_line = line.split('(')
-        gate = split_line[0]
-        return 1
 """
 Gets and parses for the module name given a preformatted list of strings
 """
@@ -125,26 +123,31 @@ def getParseModName(first_line):
     space_split = first_split.split(' ')
     return space_split[1].strip()
 
-
+"""
+Gets the lines representing inputs from a module string
+"""
 def getModInputs(text):
     inputs = []
     for line in text[1:]:
         if (line[0:5] == 'input'):
             inputs.append(line.strip())
     return inputs
+"""
+Parses lines representing inputs
+"""
 def parseModInputs(text):
     inputs = []
     for input in text:
         if ('[' and ']' in input):
             range = input.split('[')[1].split(']')[0]
             length = int(range.split(':')[0]) - int(range.split(':')[1]) + 1
+            names = input.split(']')[1].split(', ')
         else: 
             length = 1
-            
-        name = input.split(' ')[-1].split(';')[0]
-        parsed = (length, name)
-        inputs.append(parsed)
-
+            names = input.split(' ')[1].split(', ')
+        for name in names:
+            parsed = (format(name),length)
+            inputs.append(parsed)
     return inputs
 
 
@@ -157,20 +160,26 @@ def getModOutputs(text):
             outputs.append(line.strip())
     return outputs
 def parseModOutputs(text):
-    
     outputs = []
     for output in text:
         if ('[' and ']' in output):
             range = output.split('[')[1].split(']')[0]
             length = int(range.split(':')[0]) - int(range.split(':')[1]) + 1
+            names = output.split(']')[1]
+            names = names.split(',' )
         else: 
             length = 1
-            
-        name = output.split(' ')[-1].split(';')[0]
-        parsed = (length, name)
-        outputs.append(parsed)
-            
+            names = output.split(' ')[1].split(', ')
+        for name in names:
+            parsed = (format(name),length)
+            outputs.append(parsed)
     return outputs
+
+def format(text):
+    new = text.replace(';','').replace(',','').strip()
+    return new
+
+
 
 
 def getModWires(text):
@@ -182,16 +191,18 @@ def getModWires(text):
 def parseModWires(text):
     wires = []
     for wire in text:
+        
         if ('[' and ']' in wire):
             range = wire.split('[')[1].split(']')[0]
             length = int(range.split(':')[0]) - int(range.split(':')[1]) + 1
+            names = wire.split(']')[1]
+            names = names.split(',' )
         else: 
             length = 1
-        
-        name = wire.split(' ')[-1].split(';')[0]
-        parsed = (length, name)
-        wires.append(parsed)
-            
+            names = wire.split(' ')[1].split(', ')
+        for name in names:
+            parsed = (format(name),length)
+            wires.append(parsed)
     return wires
 
 def getModAssigns(text): 
@@ -202,6 +213,8 @@ def getModAssigns(text):
     return assigns
 def parseModAssigns(text):
     pass
+
+
 
 def getModInnerModules(text):
     modules = []
@@ -222,8 +235,10 @@ def parseModInnerModules(text):
     modules = []
     for module in text:
         count = module.count(',')
-        name = module.split('(')[0]
-        modules.append((count, name))
+        name = module.split('(')[0].strip()
+        if (len(name.split(' ')) == 2):
+            name = name.split(' ')[0]
+        modules.append((name, count))
     return modules
     
 
@@ -272,13 +287,50 @@ def __main__():
     filename = input("Input the name of the .v file you wish to analyze: ")
     modules = parseInputFile(filename)
 
-   
 
+   # Get all of the modules into a set
+    parsed_modules = set()
     for mod in modules:
         current_mod = module(mod)
-        current_mod.getTransCount()
+        # TEMPORARILY REMOVED FOR TESTING
+        # print(current_mod)
+        parsed_modules.add(current_mod)
 
-        print(current_mod)
+    for mod in parsed_modules:
+        print(mod)
+
+    analyzed_modules = []
+
+    #
+    while (not len(parsed_modules) == 0):
+
+        current = parsed_modules.pop()
+        current_inner_modules = current.getModules()
+        
+        known_modules = list(known_input_dependent.keys())
+        known_modules += list(known_input_independent.keys())
+        known_modules += list(inner_modules.keys())
+        ready = True
+        
+        #
+        for (name, _) in current_inner_modules:
+            
+            #
+            if name.strip() not in known_modules:
+                ready = False
+
+        #
+        if ready: 
+            current.getTransCount()
+            analyzed_modules.append(current)
+        #
+        else: 
+            parsed_modules.add(current)
+    #
+    for mod in analyzed_modules:
+        print(mod)
+        
+    
 
 
 if __name__ == "__main__":
